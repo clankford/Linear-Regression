54f470b14696821fc8834a2a895ff93eada9b9a3
Chris Lankford <christopherlankford@gmail.com>
2017-07-20 08:49:48 -0700

    Added optimizing the linear regression by a shrinkage value

--- LinearRegression.py
+++ LinearRegression.py
@@ -3,8 +3,22 @@ import numpy as np
 import sympy as sp
 import math as math
 
+# TODO: Refactor/clean up code
+
 # Display Log Values
-isLoggingOn = True
+is_logging_on = True
+
+# optimize_by = 0 = Order
+# optimize_by = 1 = Penalty
+optimize_by = 0
+
+# Order of the regression (x, x**2, x**3, etc).
+# Create the range of orders that should be tested
+# Ensure this is set to an array with only 1 value when doing optimization by penalty
+order_range = np.linspace(1, 9, 9)
+
+# ln <lambda> = -18. Lambda is a tuning variable to help penalize very large values of w to avoid over fitting.
+ln_lambda_range = np.linspace(-50, -15, 8)
 
 # Total number of data points
 count_data_points = 10
@@ -23,10 +37,6 @@ noise = np.random.normal(mu, sigma, count_data_points)
 target_data_pattern = "np.sin(2*np.pi*x)"
 # target_data_pattern = "4*x**3-2*x**2"
 
-# Order of the regression (x, x**2, x**3, etc).
-# Create the range of orders that should be tested
-order_range = np.linspace(1, 9, 9)
-
 # Array to store a RegressionSolution class for each degree order in order_range
 arr_regression_solutions = []
 
@@ -37,9 +47,10 @@ x_axis_range = 1
 
 # Class to hold the contents of the regression solution
 class RegressionSolution:
-    def __init__(self, regression_expression, order_val, arr_weights, train_rms_val, test_rms_val):
+    def __init__(self, regression_expression, order_val, lambda_val, arr_weights, train_rms_val, test_rms_val):
         self.regression_expression = regression_expression
         self.order = order_val
+        self.lam = lambda_val
         self.weights = arr_weights
         self.training_root_mean_square = train_rms_val
         self.testing_root_mean_square = test_rms_val
@@ -61,15 +72,21 @@ def generate_data(d_size, d_range, d_pattern):
 
 # Generate the an array containing the values to be summed in the error function
 # Example form: (1/2)*((w1 + w2*x1 - t1)^2 + (w1 + w2*x2 - t2)^2) ...
-def generate_error_function(arr_weights, arr_data):
+def generate_error_function(arr_weights, arr_data, lambda_val):
     expression = ""
+    norm_w = ""
+    # Generate components of the error expression
     for j in range(0, len(arr_weights)):
         if j == 0:
             expression = expression + str(arr_weights[j])
+            # Don't include w0 in this expression.
+            # norm_w = norm_w + str(arr_weights[j]) + "**2"
         if j == 1:
             expression = expression + " + " + str(arr_weights[j]) + " * {0}"
+            norm_w = norm_w + str(arr_weights[j]) + "**2"
         if j > 1:
             expression = expression + " + " + str(arr_weights[j]) + " * {0}**" + str(j)
+            norm_w = norm_w + " + " + str(arr_weights[j]) + "**2"
 
     arr_inner_error_funcs = []
     for j in range(0, len(arr_data)):
@@ -85,7 +102,12 @@ def generate_error_function(arr_weights, arr_data):
             err_func = err_func + " + " + arr_inner_error_funcs[j]
     err_func = err_func + ")"
 
-    return err_func
+    if lambda_val is None:
+        err_func = err_func
+    else:
+        err_func = err_func + " + " + str(np.exp(lambda_val)/2) + " * (" + norm_w + ")"
+
+    return str(err_func)
 
 
 # Generate the full regression expression
@@ -143,71 +165,113 @@ def plot_solutions(d_range, expression, t_pattern, t_data):
     plt.show()
 
 
+def optimization(arr_orders, arr_ln_lambda, optimize_val, training_data):
+
+    loop_array = []
+    if optimize_val == 0:
+        loop_array = arr_orders
+    elif optimize_val == 1:
+        loop_array = arr_ln_lambda
+
+    for o in range(0, len(loop_array)):
+
+        # Declare symbols
+        weights = []
+        if optimize_val == 0:
+            weights = sp.symbols('w0:%d' % (arr_orders[o] + 1))
+        elif optimize_val == 1:
+            weights = sp.symbols('w0:%d' % (arr_orders[0] + 1))
+
+        error_function = ""
+        if optimize_val == 0:
+            # Create the error function with weight variables and training data values
+            error_function = generate_error_function(weights, training_data, None)
+            print(error_function)
+        elif optimize_val == 1:
+            # Pass in the current Lambda value from loop_array
+            error_function = generate_error_function(weights, training_data, arr_ln_lambda[o])
+
+        # Evaluate the derivatives of the error function with respect to both w0 and w1
+        derivative_arr_w = []
+        for j in range(0, len(weights)):
+            derivative_arr_w.append(sp.diff(error_function, weights[j]))
+            # print("Derivative of error function with respect to w" + str(j) + ":")
+            # print(derivative_arr_w[j])
+            # print("")
+
+        # Solve the system of equations for w0 and w1
+        system_solution = sp.linsolve(derivative_arr_w, weights)
+
+        # Extract the w0, w1 values from the linear solution set
+        arr_weight_values = []
+        for w in range(0, len(weights)):
+            arr_weight_values.append(list(system_solution)[0][w])
+
+        # Generate the regression expression
+        output_regression = generate_regression_expression(arr_weight_values)
+
+        # Evaluate the root-mean-square error
+        # Erms=(2*E(w*)/N)**(1/2)
+        train_error = eval(generate_error_function(arr_weight_values, training_data, None))
+        train_erms = math.sqrt((2 * train_error) / count_data_points)
+        test_error = eval(generate_error_function(arr_weight_values, test_data, None))
+        test_erms = math.sqrt((2 * test_error) / count_data_points)
+
+        if is_logging_on:
+            print("Values for all w when solving the system of derivatives set to 0:")
+            print(system_solution)
+            print("")
+            print("Equation for the regression:")
+            print("y = " + output_regression)
+            print("")
+            if optimize_by == 0:
+                print("Training - Root-mean-square error for M = " + str(arr_orders[o]) + ":")
+                print(train_erms)
+                print("")
+                print("Testing - Root-mean-square error for M = " + str(order_range[o]) + ":")
+                print(test_erms)
+                print("")
+            elif optimize_by == 1:
+                print("Training - Root-mean-square error for ln Lambda = " + str(arr_ln_lambda[o]) + ":")
+                print(train_erms)
+                print("")
+                print("Testing - Root-mean-square error for ln Lambda = " + str(arr_ln_lambda[o]) + ":")
+                print(test_erms)
+                print("")
+
+        if optimize_by == 0:
+            arr_regression_solutions.append(
+                RegressionSolution(output_regression, arr_orders[o], None, weights, train_erms, test_erms))
+        elif optimize_by == 1:
+            arr_regression_solutions.append(
+                RegressionSolution(output_regression, arr_orders[0], arr_ln_lambda[o], weights, train_erms, test_erms))
+
+    return arr_regression_solutions
+
 training_data = generate_data(count_data_points, x_axis_range, target_data_pattern)
 test_data = generate_data(count_data_points, x_axis_range, target_data_pattern)
 
-for o in range(0, len(order_range)):
-
-    # Declare symbols
-    weights = sp.symbols('w0:%d' % (order_range[o] + 1))
-
-    # Create the error function with weight variables and training data values
-    error_function = generate_error_function(weights, training_data)
-
-    # Evaluate the derivatives of the error function with respect to both w0 and w1
-    derivative_arr_w = []
-    for i in range(0, len(weights)):
-        derivative_arr_w.append(sp.diff(error_function, weights[i]))
-        # print("Derivative of error function with respect to w" + str(i) + ":")
-        # print(derivative_arr_w[i])
-        # print("")
-
-    # Solve the system of equations for w0 and w1
-    system_solution = sp.linsolve(derivative_arr_w, weights)
-
-    # Extract the w0, w1 values from the linear solution set
-    arr_weight_values = []
-    for i in range(0, len(weights)):
-        arr_weight_values.append(list(system_solution)[0][i])
-
-    # Generate the regression expression
-    output_regression = generate_regression_expression(arr_weight_values)
-
-    # Evaluate the root-mean-square error
-    # Erms=(2*E(w*)/N)**(1/2)
-    train_error = eval(generate_error_function(arr_weight_values, training_data))
-    train_erms = math.sqrt((2*train_error)/count_data_points)
-    test_error = eval(generate_error_function(arr_weight_values, test_data))
-    test_erms = math.sqrt((2*test_error)/count_data_points)
-
-    if isLoggingOn:
-        print("Values for all w when solving the system of derivatives set to 0:")
-        print(system_solution)
-        print("")
-        print("Equation for the regression:")
-        print("y = " + output_regression)
-        print("")
-        print("Training - Root-mean-square error for M = " + str(order_range[o]) + ":")
-        print(train_erms)
-        print("")
-        print("Testing - Root-mean-square error for M = " + str(order_range[o]) + ":")
-        print(test_erms)
-        print("")
-
-    arr_regression_solutions.append(
-        RegressionSolution(output_regression, order_range[o], weights, train_erms, test_erms))
+solutions = []
+if optimize_by == 0:
+    # Do Order optimization
+    solutions = optimization(order_range, None, optimize_by, training_data)
+elif optimize_by == 1:
+    # Do Penalty optimization
+    solutions = optimization(order_range, ln_lambda_range, optimize_by, training_data)
 
 # TODO: Implement sortable object
 best_solution = None
 arr_test_rms = []
 arr_train_rms = []
-for i in range(0, len(arr_regression_solutions)):
-    arr_test_rms.append(arr_regression_solutions[i].testing_root_mean_square)
-    arr_train_rms.append(arr_regression_solutions[i].training_root_mean_square)
+for i in range(0, len(solutions)):
+    arr_test_rms.append(solutions[i].testing_root_mean_square)
+    arr_train_rms.append(solutions[i].training_root_mean_square)
     if best_solution is None:
-        best_solution = arr_regression_solutions[i]
-    elif arr_regression_solutions[i].testing_root_mean_square < best_solution.testing_root_mean_square:
-        best_solution = arr_regression_solutions[i]
+        best_solution = solutions[i]
+    elif solutions[i].testing_root_mean_square < best_solution.testing_root_mean_square:
+        best_solution = solutions[i]
+
+# TODO: Introduce lambda optimization plotting
 
 print("Best Order: " + str(best_solution.order))
 print("Best Root Mean Square: " + str(best_solution.testing_root_mean_square))
